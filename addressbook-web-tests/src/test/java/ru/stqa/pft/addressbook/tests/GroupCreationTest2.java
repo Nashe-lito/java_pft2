3f41cd022a27453a87e8e7bda1d3b41cd18d05ab
Nashe-lito <krevetk@ukr.net>
2019-01-28 17:36:00 +0200

    Параметризация тестовых методов и загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest2.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest2.java
@@ -1,25 +1,47 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData2;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTest2 extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        GroupData2 group = new GroupData2().withName("test2");
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
-        assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+            BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+            String line = reader.readLine();
+            while (line != null){
+                String[] split = line.split(";");
+                list.add(new Object[]{new GroupData2().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+                line = reader.readLine();
+            }
+            return list.iterator();
     }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData2 group) {
+            app.goTo().groupPage();
+            Groups before = app.group().all();
+            app.group().create(group);
+            assertThat(app.group().count(), equalTo(before.size() + 1));
+            Groups after = app.group().all();
+            assertThat(after, equalTo(
+                    before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        }
 
     @Test
     public void testBadGroupCreation() {
